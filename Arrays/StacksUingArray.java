158cb3e3d347362965d0eb8836dd08ba27bb597d
anniepauline <anniepauline.23@gmail.com>
2023-10-13 10:10:45 +0530

    Array Implementation of stack

---
+++ Arrays/StacksUingArray.java
@@ -0,0 +1,55 @@
+class Stack {
+
+    final int MAX_SIZE = 10;
+    int[] array = new int[MAX_SIZE];
+    int top = -1;
+
+    public static void push(int x, Stack s) {
+        if (s.top == s.MAX_SIZE - 1) {
+            System.out.println("Stac overflow");
+        }
+        s.top++;
+        s.array[s.top] = x;
+    }
+
+    public static void pop(Stack s) {
+        if (s.top == -1) {
+            System.out.println("Empty stack");
+        }
+        int data = s.array[s.top];
+        s.top--;
+        System.out.println("Popped: " + data);
+    }
+
+    public static void size(Stack s) {
+        int size = s.top + 1;
+        System.out.println("Stack size: " + size);
+    }
+
+    public static void top(Stack s) {
+        System.out.println("Top of stack:" + s.array[s.top]);
+    }
+
+    public static void print(Stack s) {
+        for (int i = 0; i <= s.top; i++) {
+            System.out.println(s.array[i]);
+        }
+    }
+}
+
+class StacksUsingArray {
+
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(2, s);
+        s.push(3, s);
+        s.push(4, s);
+        s.push(5, s);
+        s.print(s);
+        s.pop(s);
+        s.pop(s);
+        s.print(s);
+        s.size(s);
+        s.top(s);
+    }
+}
